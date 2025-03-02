afd3435e8bd57aab7778ae1aa5e8c547b6d7e458
harleen001 <harleen.johal31@gmail.com>
2025-03-03 01:28:58 +0530

    final-image-update-01:28-AM

--- src/components/neon-figure.tsx
+++ src/components/neon-figure.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Figure from '@/assets/images/figure-2.webp';
+import Figure from '@/assets/images/contactbhau.png';
 
 export default function NeonFigure() {
  return (
