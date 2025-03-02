b39409fa2982b3bd4487c9dca0c6a9723e54f961
harleen001 <harleen.johal31@gmail.com>
2025-03-03 00:33:25 +0530

    final

--- src/components/section-provider.tsx
+++ src/components/section-provider.tsx
@@ -11,7 +11,7 @@ import {
   ANIMATION_DURATION,
   SECTION_THEME_MAP,
   SECTIONS,
-  WORK_SUBSECTIONS,
+ 
 } from '@/lib/constants';
 
 export const SectionContext = createContext<SectionContextType | undefined>(
@@ -62,26 +62,18 @@ export const SectionProvider = ({
       setIsTransitioning(true);
       setCanScroll(false);
 
-      if (sectionIndex === Sections.Works) {
+      if (sectionIndex === Sections.Description) {
         setSubsectionIndex((prevSubIndex) => {
           const newSubIndex =
             direction === 'down' ? prevSubIndex + 1 : prevSubIndex - 1;
 
-          if (newSubIndex >= WORK_SUBSECTIONS) {
-            setSubsectionIndex(0);
-            setSectionIndex(Sections.Experience);
-            return 0;
-          } else if (newSubIndex < 0) {
-            setSubsectionIndex(WORK_SUBSECTIONS - 1);
-            setSectionIndex(Sections.Home);
-            return WORK_SUBSECTIONS - 1;
-          }
+          
 
           return newSubIndex;
         });
-      } else if (sectionIndex === Sections.Experience && direction === 'up') {
-        setSectionIndex(Sections.Works);
-        setSubsectionIndex(WORK_SUBSECTIONS - 1);
+      } else if (sectionIndex === Sections.Project && direction === 'up') {
+        setSectionIndex(Sections.Description);
+     
       } else {
         setSubsectionIndex(0);
         handleMainSectionChange(direction);
@@ -167,7 +159,7 @@ export const SectionProvider = ({
 
     if (
       typeof colorsMap === 'object' &&
-      sectionIndex === Sections.Works &&
+      sectionIndex === Sections.Description &&
       subsectionIndex in colorsMap
     ) {
       colors = (colorsMap as { [subKey: number]: SectionColors })[subsectionIndex];
